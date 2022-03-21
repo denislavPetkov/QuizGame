133135ed158c10ec68f4d42cc8afe2726effa457
denislav <denislav1396@gmail.com>
2022-03-21 17:57:10 +0200

    Add a way to pick the number of questions to answer

--- app/src/main/java/com/example/pmu_project/service/MessageHelperService.java
+++ app/src/main/java/com/example/pmu_project/service/MessageHelperService.java
@@ -1,19 +1,16 @@
 package com.example.pmu_project.service;
 
+import android.content.Context;
 import android.os.Handler;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 
 public interface MessageHelperService {
-    public static void showMessage(AlertDialog.Builder alertDialogBuilder, String ss, long delayMillis) {
-        alertDialogBuilder.setMessage(ss);
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                alertDialog.cancel();
-            }
-        }, delayMillis);
+    public static void ShowMessage(Context packageContext, String message) {
+        Toast.makeText(packageContext,
+                message,
+                Toast.LENGTH_LONG)
+        .show();
     }
 }
